bf88e2e53f18d459fcce6bc1ce18da94ffa016b1
dropout <dropout@dpt.wtf>
2021-11-29 19:28:01 +0100

    h2 setup

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO items (id, name, price, item_count) VALUES (1, 'tricko', 9.99, 5);
+INSERT INTO items (id, name, price, item_count) VALUES (2, 'rifle', 21.50, 3);
+INSERT INTO items (id, name, price, item_count) VALUES (3, 'sukna', 14.99, 3);
+INSERT INTO items (id, name, price, item_count) VALUES (4, 'ciapka', 7.99, 5);
+INSERT INTO items (id, name, price, item_count) VALUES (5, 'trenky', 4.50, 10);
+INSERT INTO items (id, name, price, item_count) VALUES (6, 'gatky', 3.50, 10);
+INSERT INTO items (id, name, price, item_count) VALUES (7, 'ponozky', 2.00, 20);
