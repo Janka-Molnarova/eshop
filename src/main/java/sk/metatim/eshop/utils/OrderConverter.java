39d5189e001dab2449aa843cd6b121920fb8d337
dropout <dropout@dpt.wtf>
2021-12-03 18:53:43 +0100

    converter fix

--- src/main/java/sk/metatim/eshop/utils/OrderConverter.java
+++ src/main/java/sk/metatim/eshop/utils/OrderConverter.java
@@ -3,6 +3,7 @@ package sk.metatim.eshop.utils;
 import org.springframework.stereotype.Component;
 import sk.metatim.eshop.dto.OrderRequestDTO;
 import sk.metatim.eshop.dto.OrderResponseDTO;
+import sk.metatim.eshop.helper.OrderResponseMessage;
 import sk.metatim.eshop.persistence.Order;
 
 @Component
@@ -17,11 +18,16 @@ public class OrderConverter {
     }
 
     public OrderResponseDTO convertEntityToDto(Order order) {
+
         OrderResponseDTO dto = new OrderResponseDTO();
         dto.setSuccess(order.isSuccess());
-        dto.setMessage(order.getMessage());
         dto.setOrderID(order.getOrderId());
         dto.setPrice(order.getPrice());
+
+        OrderResponseMessage orm = new OrderResponseMessage();
+        orm.setMessage(order.getMessage());
+        dto.setMessage(orm);
+
         return dto;
     }
 
