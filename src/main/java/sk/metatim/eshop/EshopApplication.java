cd806c1fbe8972bde9b2f9cdfed71689fd245705
dropout <dropout@dpt.wtf>
2021-12-15 20:51:22 +0100

    runner

--- src/main/java/sk/metatim/eshop/EshopApplication.java
+++ src/main/java/sk/metatim/eshop/EshopApplication.java
@@ -1,7 +1,16 @@
 package sk.metatim.eshop;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import sk.metatim.eshop.controller.OrderController;
+import sk.metatim.eshop.dto.OrderRequestDTO;
+import sk.metatim.eshop.dto.OrderResponseDTO;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 @SpringBootApplication
 public class EshopApplication {
@@ -10,4 +19,10 @@ public class EshopApplication {
         SpringApplication.run(EshopApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner runStuff(OrderController orderController){
+        return args -> {
+
+        };
+    }
 }
