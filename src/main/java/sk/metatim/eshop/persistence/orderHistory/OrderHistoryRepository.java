bd2f4a9debe6aa3d2b2555e279a348dc6dfe9e4d
dropout <dropout@dpt.wtf>
2021-12-22 19:50:34 +0100

    orderhistory entity & jpa

--- src/main/java/sk/metatim/eshop/persistence/orderHistory/OrderHistoryRepository.java
+++ src/main/java/sk/metatim/eshop/persistence/orderHistory/OrderHistoryRepository.java
@@ -3,4 +3,6 @@ package sk.metatim.eshop.persistence.orderHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderHistoryRepository extends JpaRepository<OrderHistory, Long> {
+
+    OrderHistory findByOrderId(String orderId);
 }
